96c408bce939266a9296d7564e149af232eae359
JakubKub13 <jakubkubala3@gmail.com>
2023-07-13 12:13:26 +0200

    adding movies to databse

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { useState, useEffect } from "react";
 const App = () => {
   const [data, setData] = useState([])
   const [error, setError] = useState(false)
+  // useState for form
+  const [moviesTitle, setMoviesTitle] = useState("")
+  const [moviesMinage, setMoviesMinage] = useState(null)
+  const [moviesTime, setMoviesTime] = useState(null)
 
   useEffect(() => {
     const unsubscribe = projectFirestore.collection('movies').onSnapshot(
@@ -29,7 +33,30 @@ const App = () => {
     projectFirestore.collection('movies').doc(id).delete()
   }
 
+ 
+
+  const submitForm = async (event) => {
+    event.preventDefault()
+    const newMovie = {title: moviesTitle, minage: moviesMinage, time: moviesTime}
+
+    try {
+      await projectFirestore.collection('movies').add(newMovie)
+      setMoviesTitle("")
+      setMoviesMinage("")
+      setMoviesTime("")
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
   return <div className="all-movies">
+    <form onSubmit={submitForm}>
+      <input type="text" onChange={ (event) => setMoviesTitle(event.target.value)} placeholder="Title" value={moviesTitle}/><br />
+      <input type="number" onChange={ (event) => setMoviesMinage(event.target.value)} placeholder="Minage" min="0" value={moviesMinage}/><br />
+      <input type="number" onChange={ (event) => setMoviesTime(event.target.value)} placeholder="Time" min="0" value={moviesTime} /><br />
+      <button type="submit" value="Add">Add</button>
+    </form>
+
     {error && <p>{error}</p>}
     {data.map((oneMovie) => {
       const {id, title, minage, time} = oneMovie
